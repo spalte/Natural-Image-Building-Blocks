e2f1f08ad9ec2515903f4b8a690059a950e4c81b
aglv <alessandro.volz@gmail.com>
2015-03-25 08:49:08 +0100

    ok

---
+++ Sources/OurPluginClass.h
@@ -0,0 +1,13 @@
+//
+//  OurPluginClass.h
+//  XMLRPC Dicom Send
+//
+//  Created by Alessandro Volz on 3/24/15.
+//  Copyright (c) 2015 volz.io. All rights reserved.
+//
+
+#import <OsiriXAPI/PluginFilter.h>
+
+@interface OurPluginClass : PluginFilter
+
+@end
