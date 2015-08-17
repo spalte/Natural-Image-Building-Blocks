91621e9478ebfff432d150412ccdfc05bc77783d
aglv <alessandro.volz@gmail.com>
2015-08-17 20:01:07 +0200

    Working on annotations storage

---
+++ NIMPR/Sources/NIJSONArchiver.h
@@ -0,0 +1,13 @@
+//
+//  NIJSONCoder.h
+//  NIMPR
+//
+//  Created by Alessandro Volz on 8/17/15.
+//  Copyright (c) 2015 volz.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NIJSONCoder : NSCoder
+
+@end
---
+++ NIMPR/Sources/NIJSONArchiver.m
@@ -0,0 +1,13 @@
+//
+//  NIJSONCoder.m
+//  NIMPR
+//
+//  Created by Alessandro Volz on 8/17/15.
+//  Copyright (c) 2015 volz.io. All rights reserved.
+//
+
+#import "NIJSONCoder.h"
+
+@implementation NIJSONCoder
+
+@end
