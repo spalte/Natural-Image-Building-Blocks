9750f649c2535cd75f6d2a572075ae6356bbee9e
aglv <alessandro.volz@gmail.com>
2016-02-09 14:16:16 +0100

    NIVolumeData asynchronous callback made simpler

--- NIBuildingBlocks/NIGenerator.m
+++ NIBuildingBlocks/NIGenerator.m
@@ -76,12 +76,12 @@ NSString * const _NIGeneratorRunLoopMode = @"_NIGeneratorRunLoopMode";
     return generatedVolume;
 }
 
-+ (NSOperation *)asynchronousRequestVolume:(NIGeneratorRequest *)request volumeData:(NIVolumeData *)volumeData queue:(NSOperationQueue *)queue handler:(void (^)(NIVolumeData* generatedVolume, NSOperation *operation))handler {
++ (NSOperation *)asynchronousRequestVolume:(NIGeneratorRequest *)request volumeData:(NIVolumeData *)volumeData queue:(NSOperationQueue *)queue handler:(void (^)(NIVolumeData* generatedVolume))handler {
     NIGeneratorOperation * operation = [[[[request operationClass] alloc] initWithRequest:request volumeData:volumeData] autorelease];
     
     if (handler)
         operation.completionBlock = ^{
-            handler(operation.generatedVolume, operation);
+            handler(operation.generatedVolume);
         };
 
     [queue addOperation:operation];
