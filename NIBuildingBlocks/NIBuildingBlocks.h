e218ce58805464e267eb8b497a142a8747600fcb
Joël Spaltenstein <spalte@naturalimage.ch>
2016-03-22 14:07:29 +0100

    removed NIAnnularPath

--- NIBuildingBlocks/NIBuildingBlocks.h
+++ NIBuildingBlocks/NIBuildingBlocks.h
@@ -46,7 +46,6 @@ FOUNDATION_EXPORT const unsigned char NIBuildingBlocksVersionString[];
 #import <NIBuildingBlocks/NIVolumeDataProperties.h>
 #import <NIBuildingBlocks/NSBezierPath+NI.h>
 #import <NIBuildingBlocks/NIAgeFormatter.h>
-#import <NIBuildingBlocks/NIAnnularPath.h>
 #import <NIBuildingBlocks/NIWindowLevelWindowWidthToolbarItem.h>
 #import <NIBuildingBlocks/NIStorage.h>
 #import <NIBuildingBlocks/OsiriXIntegration.h>
